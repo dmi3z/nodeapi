8b78342562de2fcffaa2d57f918df6c8c36058b7
Dmitry <dmi3z@yandex.ru>
2019-09-07 01:38:42 +0300

    2

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ var fs = require('fs');
 
 var app = express();
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var users = [];
 
 app.get('/users', (req, res) => {
+    console.log('GET is work');
     res.send(users);
 });
 
